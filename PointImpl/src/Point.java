70ec83dcf766e5afc763d69569281095f5804116
Markus Just <markus.just@stz-softwaretechnik.de>
2016-09-15 11:47:31 +0200

    Initial Commit PointImpl project

---
+++ PointImpl/src/Point.java
@@ -0,0 +1,54 @@
+
+public class Point extends Object{
+
+
+	private double x;
+	private double y;
+	
+	public Point(){
+		
+	}
+	
+	public Point(double x, double y){
+		
+	}
+	
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+	
+	public double norm(){
+		return 0.0;
+	}
+	
+	public void rotate(double theta) throws Exception{
+		
+	}
+	
+	public void displace(Point p){
+		
+	}
+	
+	@Override
+	public boolean equals(Object other){
+		return false;	
+	}
+	
+	@Override
+	public String toString(){
+		return null;
+	}
+
+}
